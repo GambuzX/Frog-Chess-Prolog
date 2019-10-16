ad625d2e9cd8597cac3f85ece887b8328fb534c4
Mário Gil <mg_mesquita@hotmail.com>
2019-10-16 20:54:12 +0100

    Smaller frog

--- display.pl
+++ display.pl
@@ -58,8 +58,7 @@ display_row_frogs(Row, N) :-
     write('  '), display_frog_row_2(Row, 0), nl,
     ansi_format([fg(blue)], '~w', [N]), write(' '), display_frog_row_3(Row, 0), nl,
     write('  '), display_frog_row_4(Row, 0), nl,
-    write('  '), display_frog_row_5(Row, 0), nl,
-    write('  '), display_frog_row_6(Row, 0), nl.
+    write('  '), display_frog_row_5(Row, 0), nl.
 
 
 /*
@@ -151,39 +150,21 @@ display_frog_row_5([Frog|Rest], ColN) :-
     NextCol is ColN + 1,
     display_frog_row_5(Rest, NextCol).
 
-
-/*
-
-*/
-display_frog_row_6([], _).
-display_frog_row_6([Frog|Rest], ColN) :-
-    ColN >= 0,
-    ColN < 8,
-    (
-        ColN = 0,
-        put_code(186);
-        1 = 1
-    ),
-    display_frog_ascii_6(Frog),
-    put_code(186),
-    NextCol is ColN + 1,
-    display_frog_row_6(Rest, NextCol).
-
 /*
 
 */
 display_col_head(7) :-
-    write('           '),
+    write('        '),
     ansi_format([fg(blue)], '~w', [8]),
-    write('          ').
+    write('       ').
 
 display_col_head(ColN) :-
     ColN >= 0,
     ColN < 7,
     N is ColN+1,
-    write('           '),
+    write('        '),
     ansi_format([fg(blue)], '~w', [N]),
-    write('          '),
+    write('       '),
     NextCol is ColN + 1,
     display_col_head(NextCol).
 
@@ -192,7 +173,7 @@ display_col_head(ColN) :-
 
 */
 display_top(7) :-
-    display_div_line(21),
+    display_div_line(15),
     put_code(187).
 
 display_top(ColN) :-
@@ -202,7 +183,7 @@ display_top(ColN) :-
         ColN = 0, put_code(201);
         1 = 1
     ),
-    display_div_line(21),
+    display_div_line(15),
     put_code(203),
     NextCol is ColN + 1,
     display_top(NextCol).
@@ -212,7 +193,7 @@ display_top(ColN) :-
 
 */
 display_bottom(7) :-
-    display_div_line(21),
+    display_div_line(15),
     put_code(188).
 
 display_bottom(ColN) :-
@@ -222,7 +203,7 @@ display_bottom(ColN) :-
         ColN = 0, put_code(200);
         1 = 1
     ),
-    display_div_line(21),
+    display_div_line(15),
     put_code(202),
     NextCol is ColN + 1,
     display_bottom(NextCol).
@@ -232,7 +213,7 @@ display_bottom(ColN) :-
 
 */    
 display_div(7) :-
-    display_div_line(21),
+    display_div_line(15),
     put_code(185).
 
 display_div(ColN) :-
@@ -242,7 +223,7 @@ display_div(ColN) :-
         ColN = 0, put_code(204);
         1 = 1
     ),
-    display_div_line(21),
+    display_div_line(15),
     put_code(206),
     NextCol is ColN + 1,
     display_div(NextCol).
@@ -255,75 +236,62 @@ display_div_line(Count) :-
     N is Count-1,
     display_div_line(N).
 
-
 /*
 
 */
 display_frog_ascii_1(X) :-
     X = empty,
-    write('                     ').
+    write('               ').
 
 display_frog_ascii_1(X) :-
     (X = green, Color = green; 
     X = pink, Color = magenta),
-    ansi_format([fg(Color)], '~w', ['       _     _       ']).
-
+    ansi_format([fg(Color)], '~w', ['    (\')=(\')    ']).
+    
 /*
 
 */
 display_frog_ascii_2(X) :-
     X = empty,
-    write('                     ').
+    write('               ').
 
 display_frog_ascii_2(X) :-
     (X = green, Color = green; 
     X = pink, Color = magenta),
-    ansi_format([fg(Color)], '~w', ['      (\')-=-(\')      ']).
+    ansi_format([fg(Color)], '~w', ['  __(  "  )__  ']).
     
 /*
 
 */
 display_frog_ascii_3(X) :-
     X = empty,
-    write('                     ').
+    write('               ').
 
 display_frog_ascii_3(X) :-
     (X = green, Color = green; 
     X = pink, Color = magenta),
-    ansi_format([fg(Color)], '~w', ['    __(   "   )__    ']).
+    ansi_format([fg(Color)], '~w', [' / _/\'---\'\\_ \\ ']).
     
 /*
 
 */
 display_frog_ascii_4(X) :-
     X = empty,
-    write('                     ').
+    write('               ').
 
 display_frog_ascii_4(X) :-
     (X = green, Color = green; 
     X = pink, Color = magenta),
-    ansi_format([fg(Color)], '~w', ['   / _/\'-----\'\\_ \\   ']).
+    ansi_format([fg(Color)], '~w', ['_\\\\ \\\\   // //_']).
     
 /*
 
 */
 display_frog_ascii_5(X) :-
     X = empty,
-    write('                     ').
+    write('               ').
 
 display_frog_ascii_5(X) :-
     (X = green, Color = green; 
     X = pink, Color = magenta),
-    ansi_format([fg(Color)], '~w', ['___\\\\ \\\\     // //___']).
-    
-/*
-
-*/
-display_frog_ascii_6(X) :-
-    X = empty,
-    write('                     ').
-
-display_frog_ascii_6(X) :-
-    (X = green, Color = green; 
-    X = pink, Color = magenta),
-    ansi_format([fg(Color)], '~w', ['>____)/_\\---/_\\(____<']).
+    ansi_format([fg(Color)], '~w', ['>__)/_\\-/_\\(__<']).
