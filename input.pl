f02770522a481b386739c512aac446fd227f6447
Mário Gil <mg_mesquita@hotmail.com>
2019-10-25 09:11:28 +0100

    Method for reading position

--- input.pl
+++ input.pl
@@ -11,6 +11,8 @@ readRest(Ch, [Ch | Rest]) :-
     get0(Ch1),
     readRest(Ch1, Rest).
 
+    
+
 readInteger(Int) :-
     readInput(Int),
     integer(Int).
@@ -22,14 +24,40 @@ readChar(Char) :-
     name(Char, [Ch]),
     readRest(Char, _).
 
+
+
+readSingleChar(Char) :-
+    get_single_char(Ch),
+    (Ch >= 97, Ch =< 122 ;
+    Ch >= 65, Ch =< 90),
+    name(Char, [Ch]).
+
+readSingleInteger(Int) :-
+    get_single_char(Ch),
+    Ch >= 48,
+    Ch =< 57,
+    name(Int, [Ch]).
+
+
+
 readCol(Col) :-
-    readInteger(Col),
-    Col >= 0,
-    Col =< 7.
+    readSingleInteger(Col),
+    Col >= 1,
+    Col =< 8.
 
 readRow(Row) :-
-    readChar(Row),
+    readSingleChar(Row),
     char_type(Row, alpha),
     char_code(Row, Code),
     Code >= 97,
     Code =< 104.
+
+readPosition(Row, Col) :-
+    write('Position: ('),
+    readRow(Row),
+    write(Row),
+    put_char(','),
+    readCol(Col),
+    write(Col),
+    put_char(')'),
+    nl.
